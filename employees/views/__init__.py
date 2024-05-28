d38a097e4ff1a42cf10a09cf33fb0a41078ab7e7
Ahmed Osama <a.osama9392@gmail.com>
2024-05-28 13:02:32 +0300

    Update __init__.py

--- employees/views/__init__.py
+++ employees/views/__init__.py
@@ -1,12 +1,28 @@
 from employees.views.company_views import CompanyListViewSet, CompanyCreateViewSet, CompanyRetrieveViewSet, \
     CompanyUpdateViewSet, CompanyDestroyViewSet
 
+from employees.views.department_views import DepartmentListViewSet, DepartmentCreateViewSet, DepartmentRetrieveViewSet, \
+    DepartmentUpdateViewSet, DepartmentDestroyViewSet
+
+from employees.views.employee_views import EmployeeListViewSet, EmployeeCreateViewSet, EmployeeRetrieveViewSet, \
+    EmployeeUpdateViewSet, EmployeeDestroyViewSet
+
 __all__ = (
     "CompanyListViewSet",
     "CompanyCreateViewSet",
     "CompanyRetrieveViewSet",
     "CompanyUpdateViewSet",
     "CompanyDestroyViewSet",
+
+    "EmployeeListViewSet",
+    "EmployeeCreateViewSet",
+    "EmployeeRetrieveViewSet",
+    "EmployeeUpdateViewSet",
+    "EmployeeDestroyViewSet",
+
+    "DepartmentListViewSet",
+    "DepartmentCreateViewSet",
+    "DepartmentRetrieveViewSet",
+    "DepartmentUpdateViewSet",
+    "DepartmentDestroyViewSet",
 )
-
-
